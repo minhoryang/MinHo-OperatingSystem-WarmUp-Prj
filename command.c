ad06c8f19fbeaa8778ff113c445e699d3619c116
minhoryang <minhoryang@gmail.com>
2012-09-19 21:30:31 +0900

    (19/36) 비트맵끝났다+_+! Running test: bitmap_all ... pass Running test: bitmap_any ... pass Running test: bitmap_contains ... pass Running test: bitmap_count ... pass Running test: bitmap_dump ... pass Running test: bitmap_flip ... pass Running test: bitmap_mark ... pass Running test: bitmap_none ... pass Running test: bitmap_reset ... pass Running test: bitmap_scan ... pass Running test: bitmap_scan_and_flip ... pass Running test: bitmap_set ... pass Running test: bitmap_set_all ... pass Running test: bitmap_set_multiple ... pass Running test: bitmap_size ... pass Running test: bitmap_test ... pass Running test: hash_apply ... fail Running test: hash_delete ... fail Running test: hash_etc ... fail Running test: hash_find ... fail Running test: hash_insert ... fail Running test: hash_plain ... pass Running test: hash_replace ... fail Running test: list_frontback ... fail Running test: list_insert ... fail Running test: list_insert_ordered ... fail Running test: list_maxminsizeempty ... fail Running test: list_plain ... pass Running test: list_pops ... fail Running test: list_pushes ... fail Running test: list_remove ... fail Running test: list_reverse ... fail Running test: list_sort ... fail Running test: list_splice ... fail Running test: list_unique ... fail Result: 19 / 36

--- command.c
+++ command.c
@@ -93,22 +93,22 @@ void lCommandsHandler(struct list *L, char **toked, int tokin){
 		"hash_size",	// 23
 		"hash_empty",	// 24
 		"hash_apply",	// 25
-		"bitmap_size",	// 26
-		"bitmap_set",	// 27
+		"bitmap_size",	// 26 (ok)
+		"bitmap_set",	// 27 (ok)
 		"bitmap_mark",	// 28 (ok)
-		"bitmap_reset",	// 29
-		"bitmap_flip",	// 30
+		"bitmap_reset",	// 29 (ok)
+		"bitmap_flip",	// 30 (ok)
 		"bitmap_test",	// 31 (ok)
-		"bitmap_set_all",	// 32
-		"bitmap_set_multiple",	// 33
-		"bitmap_count",	// 34
-		"bitmap_contains",	// 35
-		"bitmap_any",	// 36
-		"bitmap_none",	// 37
+		"bitmap_set_all",	// 32 (ok)
+		"bitmap_set_multiple",	// 33 (ok)
+		"bitmap_count",	// 34 (ok)
+		"bitmap_contains",	// 35 (ok)
+		"bitmap_any",	// 36 (ok)
+		"bitmap_none",	// 37 (ok)
 		"bitmap_all",	// 38 (ok)
-		"bitmap_scan",	// 39
-		"bitmap_scan_and_flip",	// 40
-		"bitmap_dump"	// 41
+		"bitmap_scan",	// 39 (ok)
+		"bitmap_scan_and_flip",	// 40 (ok)
+		"bitmap_dump"	// 41 (work...)
 	};
 	struct my_head *target;
 	int type;
@@ -119,18 +119,106 @@ void lCommandsHandler(struct list *L, char **toked, int tokin){
 	switch(StringSwitch(&rule, rule_size, toked[0])){
 		case 0:  // 
 			break;
+		case 26:  // bitmap_size
+			if(tokin<2 || type==2)
+				printf("%d\n",
+					bitmap_size((struct bitmap *)target->data)
+				);
+			break;
+		case 27:  // bitmap_set
+			if(tokin<4 || type==2)
+				bitmap_set(
+					(struct bitmap *)target->data,
+					atoi(toked[2]),
+					!strcmp(toked[3], "true") ? 1 : 0 
+				);
+			break;
 		case 28:  // bitmap_mark
 			if(tokin<3 || type==2)
 				bitmap_mark(
-						(struct bitmap *)target->data,
-						atoi(toked[2]));
+					(struct bitmap *)target->data,
+					atoi(toked[2])
+				);
+			break;
+		case 29:  // bitmap_reset
+			if(tokin<3 || type==2)
+				bitmap_reset(
+					(struct bitmap *)target->data,
+					atoi(toked[2])
+				);
+			break;
+		case 30:  // bitmap_flip
+			if(tokin<3 || type==2)
+				bitmap_flip(
+					(struct bitmap *)target->data,
+					atoi(toked[2])
+				);
 			break;
 		case 31:  // bitmap_test
 			if(tokin<3 || type==2)
 				printf("%s\n",
 					bitmap_test(
+						(struct bitmap *)target->data,
+						atoi(toked[2])
+					) ? "true" : "false"
+				);
+			break;
+		case 32:  // bitmap_set_all
+			if(tokin<3 || type==2)
+				bitmap_set_all(
 					(struct bitmap *)target->data,
-					atoi(toked[2])) ? "true" : "false"
+					!strcmp(toked[2], "true") ? 1 : 0 
+				);
+			break;
+		case 33:  // bitmap_set_multiple
+			if(tokin<5 || type==2)
+				bitmap_set_multiple(
+					(struct bitmap *)target->data,
+					atoi(toked[2]),
+					atoi(toked[3]),
+					!strcmp(toked[4], "true") ? 1 : 0 
+				);
+			break;
+		case 34:  // bitmap_count
+			if(tokin<5 || type==2)
+				printf("%d\n",
+					bitmap_count(
+						(struct bitmap *)target->data,
+						atoi(toked[2]),
+						atoi(toked[3]),
+						!strcmp(toked[4], "true") ? 1 : 0
+					)
+				);
+			break;
+		case 35:  // bitmap_contains
+			if(tokin<5 || type==2)
+				printf("%s\n",
+					bitmap_contains(
+						(struct bitmap *)target->data,
+						atoi(toked[2]),
+						atoi(toked[3]),
+						!strcmp(toked[4], "true") ? 1 : 0
+					) ? "true" : "false"
+				);
+			break;
+		case 36:  // bitmap_any
+			if(tokin<4 || type==2)
+				printf("%s\n",
+					bitmap_any(
+						(struct bitmap *)target->data,
+						atoi(toked[2]),
+						atoi(toked[3])
+					) ? "true" : "false"
+				);
+			break;
+		case 37:  // bitmap_none
+			if(tokin<4 || type==2)
+				printf("%s\n",
+					bitmap_none(
+						(struct bitmap *)target->data,
+						atoi(toked[2]),
+						atoi(toked[3])
+					) ? "true" : "false"
 				);
 			break;
 		case 38:  // bitmap_all
@@ -139,7 +227,36 @@ void lCommandsHandler(struct list *L, char **toked, int tokin){
 					bitmap_all(
 						(struct bitmap *)target->data,
 						atoi(toked[2]),
-						atoi(toked[3])) ? "true" : "false"
+						atoi(toked[3])
+					) ? "true" : "false"
+				);
+			break;
+		case 39:  // bitmap_scan
+			if(tokin<5 || type==2)
+				printf("%u\n",
+					bitmap_scan(
+						(struct bitmap *)target->data,
+						atoi(toked[2]),
+						atoi(toked[3]),
+						!strcmp(toked[4], "true") ? 1 : 0
+					)
+				);
+			break;
+		case 40:  // bitmap_scan_and_flip
+			if(tokin<5 || type==2)
+				printf("%u\n",
+					bitmap_scan_and_flip(
+						(struct bitmap *)target->data,
+						atoi(toked[2]),
+						atoi(toked[3]),
+						!strcmp(toked[4], "true") ? 1 : 0
+					)
+				);
+			break;
+		case 41:  // bitmap_dump
+			if(tokin<2 || type==2)
+				bitmap_dump(
+					(struct bitmap *)target->data
 				);
 			break;
 		case -1:
